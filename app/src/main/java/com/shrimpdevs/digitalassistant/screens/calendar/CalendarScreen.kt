e0dcf301a0146e627f8821f899c4068b9346143e
Dnilson Achahuanco Huarilloclla <dnilson.achahuanco@tecsup.edu.pe>
2025-06-21 00:11:18 -0500

    Obetner ubicacion del usuario

--- app/src/main/java/com/shrimpdevs/digitalassistant/screens/calendar/CalendarScreen.kt
+++ app/src/main/java/com/shrimpdevs/digitalassistant/screens/calendar/CalendarScreen.kt
@@ -1,2 +1,40 @@
 package com.shrimpdevs.digitalassistant.screens.calendar
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.shrimpdevs.digitalassistant.ui.theme.DarkBlue
+import com.shrimpdevs.digitalassistant.ui.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CalendarScreen(
+    titulo: String,
+    contenido: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = titulo, color = White) },
+                // Puedes personalizar más la barra aquí
+                containerColor = DarkBlue
+            )
+        },
+        containerColor = DarkBlue // Fondo del Scaffold
+    ) { innerPadding ->
+        androidx.compose.foundation.layout.Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DarkBlue)
+                .padding(innerPadding)
+        ) {
+            contenido()
+        }
+    }
+}
